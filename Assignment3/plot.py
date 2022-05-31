0ccb0009c68ea404dcef18d8ce258ef62e9fb096
KaikaiLinHanze <94834233+KaikaiLinHanze@users.noreply.github.com>
2022-05-31 11:33:55 +0200

    add description and label of plot.py

--- Assignment3/plot.py
+++ Assignment3/plot.py
@@ -1,3 +1,12 @@
+"""
+file name:plot.py
+file author: Kai-kai Lin
+Date: 2022.05.31
+Description:
+Plot the same job in different number of cores.
+X: cores
+y: time-consuming
+"""
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,4 +21,6 @@ with open("output/timings.txt") as r:
 y = np.array(y)
 x = np.array(x)
 plt.plot(x,y)
+plt.xlabel("core(s)")
+plt.ylabel("time")
 plt.savefig("output/timings.png")
